585081015aef3d9af4683eea44c1be816214a636
D4rkisek <maruf.f@hotmail.com>
2024-03-10 16:38:46 +0000

    create page for viewing users data

--- admins/views.py
+++ admins/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 from register.models import Member
 
+def admin(request):
+    all_members = Member.objects.all
+    return render(request, 'admin/admin.html', {'all': all_members})
 
