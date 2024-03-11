5a8260071844e7164bc9c8cb9dc7421e8969f9c8
D4rkisek <maruf.f@hotmail.com>
2024-03-11 19:03:17 +0000

    add login page & connect submitted form into db

--- register/views.py
+++ register/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
+from .forms import MemberForm
+from django.contrib import messages
 
 def register(request):
-    return render(request, 'register/register.html')
+    # If user submits the form on the website
+    if request.method == "POST":
+        form = MemberForm(request.POST or None)
+        if form.is_valid():
+            # Save the submitted data into our db
+            form.save()
+            messages.success(request, 'You have successfully registered!')
+            return redirect('login')
+    else:
+        return render(request, 'register/register.html', {})
 
+def login(request):
+    return render(request, 'register/login.html', {})
