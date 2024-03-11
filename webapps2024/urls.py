bfafb20020f892fa1d5def094740f2327e559640
D4rkisek <maruf.f@hotmail.com>
2024-03-11 13:46:35 +0000

    create an index page

--- webapps2024/urls.py
+++ webapps2024/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="Home page"),
     path('', include('admins.urls')),
     path('', include('register.urls')),
 ]
